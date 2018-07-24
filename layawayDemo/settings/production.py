af1cab6e37b2165635cde06b2eb8a422c1081d02
Robbie Robinson <haki.projects@gmail.com>
2018-07-24 11:30:47 -0700

    added a templates folder to the default app

--- layawayDemo/settings/production.py
+++ layawayDemo/settings/production.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'layawayDemo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'layawayDemo/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
